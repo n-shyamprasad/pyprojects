c53b117b3d248722b8284209f2c08cfee04fd6a8
shyamprasad <shyamprasad>
2022-11-08 18:53:39 +0800

    Day-1: 08Nov22 - basics

---
+++ basic/check_palindrome.py
@@ -0,0 +1,16 @@
+def palindrome(num):
+    print("Orgnial NUmber: ", num)
+    org_num = num
+    rev_num = 0
+    while num > 0:
+        rem = num % 10
+        rev_num = (rev_num * 10) + rem
+        num = num // 10
+    if org_num == rev_num:
+        return "is palindrome"
+    else:
+        return "is not palindrome"
+
+
+print(palindrome(242))
+print(palindrome(112))
